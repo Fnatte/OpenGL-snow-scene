f1b433e8f360b0c7027aeee1e04bdf5b4b59d951
Rovanion Luckey <rovanion.luckey@gmail.com>
2015-11-02 14:40:25 +0100

    Refactored camera system to make multiple cameras possible.

--- camera.c
+++ camera.c
@@ -1,102 +1,118 @@
+#include <math.h>
+
 #include "./libraries/GLUtilities.h"
 #include "./libraries/VectorUtils3.h"
+#include "camera.h"
 
 
-mat4 projectionMatrix;
 mat4 lookMatrix;
-vec3 cameraPos;
-vec3 cameraTarget;
-vec3 cameraNormal;
-vec3 cameraDirection;
-
+struct Camera userCamera;
 
 void initCamera() {
-	cameraPos = (vec3){1.5f, 20.0f, -50.0f};
-	cameraTarget = (vec3){10.0f, 15.0f, 5.0f};
-	cameraNormal = (vec3){0.0f, 1.0f, 0.0f};
-	lookMatrix = lookAtv(cameraPos, cameraTarget, cameraNormal);
+	userCamera.position = (vec3){1.5f, 20.0f, -50.0f};
+	userCamera.target = (vec3){10.0f, 15.0f, 5.0f};
+	userCamera.normal = (vec3){0.0f, 1.0f, 0.0f};
+	lookMatrix = lookAtv(userCamera.position, userCamera.target, userCamera.normal);
 }
 
 
-vec3 moveCameraOnKeyboard(vec3 in, vec3 cameraNormal, vec3 cameraDirection) {
+struct Camera moveCameraOnKeyboard(struct Camera c) {
 	vec3 forward;
 	vec3 leftV;
 	if(keyIsDown('x')){
-		forward = ScalarMult(cameraDirection, 10.0f);
-		leftV = ScalarMult(CrossProduct(cameraDirection, cameraNormal), 10.0f);
+		forward = ScalarMult(cameraDirection(c), 10.0f);
+		leftV = ScalarMult(CrossProduct(cameraDirection(c), c.normal), 10.0f);
 	}
 	else{
-		forward = ScalarMult(cameraDirection, 1.0f);
-		leftV = ScalarMult(CrossProduct(cameraDirection, cameraNormal), 1.0f);
+		forward = ScalarMult(cameraDirection(c), 1.0f);
+		leftV = ScalarMult(CrossProduct(cameraDirection(c), c.normal), 1.0f);
 	}
 
 	if(keyIsDown('w')) {
-		in.x += forward.x;
-		in.y += forward.y;
-		in.z += forward.z;
+		c.position.x += forward.x;
+		c.target.x += forward.x;
+		c.position.y += forward.y;
+		c.target.y += forward.y;
+		c.position.z += forward.z;
+		c.target.z += forward.z;
 	}
 	else if (keyIsDown('s')) {
-		in.x -= forward.x;
-		in.y -= forward.y;
-		in.z -= forward.z;
+		c.position.x -= forward.x;
+		c.target.x -= forward.x;
+		c.position.y -= forward.y;
+		c.target.y -= forward.y;
+		c.position.z -= forward.z;
+		c.target.z -= forward.z;
 	}
 
 	if(keyIsDown('a')){
-		in.x -= leftV.x;
-		in.y -= leftV.y;
-		in.z -= leftV.z;
+		c.position.x -= leftV.x;
+		c.target.x -= leftV.x;
+		c.position.y -= leftV.y;
+		c.target.y -= leftV.y;
+		c.position.z -= leftV.z;
+		c.target.z -= leftV.z;
 	}
 	else if(keyIsDown('d')){
-		in.x += leftV.x;
-		in.y += leftV.y;
-		in.z += leftV.z;
+		c.position.x += leftV.x;
+		c.target.x += leftV.x;
+		c.position.y += leftV.y;
+		c.target.y += leftV.y;
+		c.position.z += leftV.z;
+		c.target.z += leftV.z;
 	}
 
-	if(keyIsDown('q'))
-		in.y += 0.1;
-	else if(keyIsDown('e'))
-		in.y -= 0.1;
+	if(keyIsDown('q')) {
+		c.position.y += 0.1;
+		c.target.y += 0.1;
+	}
+	else if(keyIsDown('e')) {
+		c.position.y -= 0.1;
+		c.target.y -= 0.1;
+	}
 
 	printError("moveonkeyinputrelativecamera()");
-	return in;
+	return c;
 }
 
 
-vec3 smoothRandomMovement(vec3 in, float magnitude) {
+struct Camera smoothRandomMovement(struct Camera c, float magnitude) {
 	GLfloat time = (GLfloat)glutGet(GLUT_ELAPSED_TIME) / 1000;
-
+	c.target.y += sin(time) * magnitude;
+	return c;
 }
 
 
 void handleMouse(int x, int y) {
 	int width = glutGet(GLUT_WINDOW_WIDTH);
 	int height = glutGet(GLUT_WINDOW_HEIGHT);
-	cameraTarget = (vec3)
+	userCamera.target = (vec3)
 		{cos((float)x / width * M_PI * 2) * sin((float)y / height * M_PI),
 		 -(float)y / height + 0.5,
 		 sin((float)x / width * M_PI * 2) * sin((float)y / height * M_PI)};
-	cameraTarget = VectorAdd(cameraTarget, cameraPos);
+	userCamera.target = VectorAdd(userCamera.target, userCamera.position);
 
-	lookMatrix = lookAtv(cameraPos, cameraTarget, cameraNormal);
-	cameraDirection = Normalize(VectorSub(cameraTarget, cameraPos));
+	lookMatrix = lookAtv(userCamera.position, userCamera.target, userCamera.normal);
 	printError("handleMouse()");
 }
 
 
 void reshape(GLsizei w, GLsizei h) {
 	glViewport(0, 0, w, h);
-	projectionMatrix = perspective(90, (GLfloat)w/(GLfloat)h, 0.1, 1000);
+	userCamera.projection = perspective(90, (GLfloat)w/(GLfloat)h, 0.1, 1000);
 }
 
 
 mat4 getProjectionViewMatrix() {
-	cameraPos = moveCameraOnKeyboard(cameraPos, cameraNormal, cameraDirection);
-	cameraTarget = moveCameraOnKeyboard(cameraTarget, cameraNormal, cameraDirection);
-
-	lookMatrix = lookAtv(cameraPos, cameraTarget, cameraNormal);
-	return Mult(projectionMatrix, lookMatrix);
+	userCamera = moveCameraOnKeyboard(userCamera);
+	lookMatrix = lookAtv(userCamera.position, userCamera.target, userCamera.normal);
+	return Mult(userCamera.projection, lookMatrix);
 }
 
 vec3 getCameraPos() {
-	return cameraPos;
+	return userCamera.position;
+}
+
+vec3 cameraDirection(struct Camera c) {
+	return Normalize(VectorSub(c.target, c.position));
 }
--- camera.h
+++ camera.h
@@ -1,13 +1,24 @@
+#include "./libraries/VectorUtils3.h"
+
+
+struct Camera {
+	vec3 position;
+	vec3 normal;
+	vec3 target;
+	mat4 projection;
+};
+
+
 /**
  * Move the camera around based on keyboard input.
  */
-vec3 moveCameraOnKeyboard(vec3 in, vec3 cameraNormal, vec3 cameraDirection);
+struct Camera moveCameraOnKeyboard(struct Camera c);
 
 
 /**
  * Introduce a camera shake with the specified magnitude.
  */
-vec3 smoothRandomMovement(vec3 in, float magnitude);
+struct Camera smoothRandomMovement(struct Camera in, float magnitude);
 
 void initCamera();
 
@@ -18,3 +29,5 @@ void reshape(GLsizei w, GLsizei h);
 mat4 getProjectionViewMatrix();
 
 vec3 getCameraPos();
+
+vec3 cameraDirection(struct Camera c);
