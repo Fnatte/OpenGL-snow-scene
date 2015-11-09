77e9d15012c0a27ddc9b4b46dc7845d24023a131
Rovanion Luckey <rovanion.luckey@gmail.com>
2015-11-09 15:53:43 +0100

    Missed ground.c in last commit.

--- ground.c
+++ ground.c
@@ -25,10 +25,10 @@ void initializeGround(Model *_model, GLuint _program) {
 void drawGround(mat4 matrix) {
 	glUseProgram(program);
 	glBindTexture(GL_TEXTURE_2D, textureGroundSpecular);
-	glUniformMatrix4fv(glGetUniformLocation(program, "projectionViewWorldTransform"), 1, GL_TRUE, matrix.m);
+	glUniformMatrix4fv(glGetUniformLocation(program, "modelViewProjectionTransform"), 1, GL_TRUE, matrix.m);
 	drawModelWithLocations(model, positionLocation, normalLocation, textureLocation);
 }
 
 void drawGroundWithProgram(GLuint program) {
-	DrawModel(model, program, "in_Position", NULL, NULL);
+	DrawModel(model, program, "inPosition", NULL, NULL);
 }
