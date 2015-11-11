9deded6b93054daa63bc82de2745c54f2d5361ca
Rovanion Luckey <rovanion.luckey@gmail.com>
2015-11-11 16:29:04 +0100

    Added a correct main.h

--- main.h
+++ main.h
@@ -1,11 +1,18 @@
-/**
- * Draws the object with the specified transform.
- *
- * @param transform The transform to be applied to the model.
- * @param model The model to be drawn.
- */
-void drawObject(mat4 transform, Model* model, GLuint p);
+#ifndef MAIN_H
+#define MAIN_H
 
+
+GLuint fullProgram, plainProgram, instancingProgram, skyboxProgram;
+
+void reshapeViewport(GLsizei w, GLsizei h);
+void initUserCamera();
+void initpointLight();
+void rotateLight();
+mat4 getShadowMapTransform(mat4 modelViewProjectionTransform);
+void renderScene(void);
 void handleMouse(int x, int y);
+void onTimer(int value);
+int main(int argc, char** argv);
 
-GLuint fullProgram, plainProgram, instancingProgram;
+
+#endif
